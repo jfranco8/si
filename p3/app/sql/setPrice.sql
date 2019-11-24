5789026b7fb03f3c79af207ee9238ac5a16ea444
martagm1 <martagm_97@hotmail.com>
2019-11-24 13:09:20 +0100

    setPrice.sql

---
+++ p3/app/sql/setPrice.sql
@@ -0,0 +1,6 @@
+update public.orderdetail
+set
+  price = orderdetail.quantity * prod_mov.price /
+  power (1.02, extract (year from now()) - extract (year from orders.orderdate))
+from orders, (products NATURAL JOIN imdb_movies) AS prod_mov
+where orders.orderid = orderdetail.orderid and prod_mov.prod_id=orderdetail.prod_id;
