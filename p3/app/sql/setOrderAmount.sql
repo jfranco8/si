43dedffe0d570e4233da28a21a8a1a6d6805202e
martagm1 <martagm_97@hotmail.com>
2019-11-25 16:41:38 +0100

    todos los sql

--- p3/app/sql/setOrderAmount.sql
+++ p3/app/sql/setOrderAmount.sql
@@ -2,8 +2,13 @@
 ----------------------------
 ----- SET NETAMOUNT & TOTALAMOUNT
 ----------------------------
-update public.orders
-set netamount = total, totalamount = netamount*(100+tax)/100
-from(select sum(price) as total, orderid
-from orderdetail group by orderid) as alias
-where alias.orderid = orders.orderid
+create or replace function setOrderAmount() retusns void as $$ begin
+  update public.orders
+  set netamount = total, totalamount = total*(100+tax)/100
+  from(select sum(price) as total, orderid
+  from orderdetail group by orderid) as alias
+  where alias.orderid = orders.orderid
+end;
+$$
+language plpgsql;
+select * from setOrderAmount()
