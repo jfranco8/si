b367fbaa678a165c44847df08e895a0326bb348f
marta garcia marin <martagm_97@hotmail.com>
2019-12-05 16:48:31 +0100

    empezando practiquita

---
+++ p4/app/database.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys, traceback, time
+
+from sqlalchemy import create_engine
+
+# configurar el motor de sqlalchemy
+db_engine = create_engine("postgresql://alumnodb:alumnodb@localhost/si1", echo=False, execution_options={"autocommit":False})
+
+def dbConnect():
+    return db_engine.connect()
+
+def dbCloseConnect(db_conn):
+    db_conn.close()
+
+def getListaCliMes(db_conn, mes, anio, iumbral, iintervalo, use_prepare, break0, niter):
+
+    # TODO: implementar la consulta; asignar nombre 'cc' al contador resultante
+    consulta = " ... "
+    
+    # TODO: ejecutar la consulta 
+    # - mediante PREPARE, EXECUTE, DEALLOCATE si use_prepare es True
+    # - mediante db_conn.execute() si es False
+
+    # Array con resultados de la consulta para cada umbral
+    dbr=[]
+
+    for ii in range(niter):
+
+        # TODO: ...
+
+        # Guardar resultado de la query
+        dbr.append({"umbral":iumbral,"contador":res['cc']})
+
+        # TODO: si break0 es True, salir si contador resultante es cero
+        
+        # Actualizacion de umbral
+        iumbral = iumbral + iintervalo
+                
+    return dbr
+
+def getMovies(anio):
+    # conexion a la base de datos
+    db_conn = db_engine.connect()
+
+    query="select movietitle from imdb_movies where year = '" + anio + "'"
+    resultproxy=db_conn.execute(query)
+
+    a = []
+    for rowproxy in resultproxy:
+        d={}
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for tup in rowproxy.items():
+            # build up the dictionary
+            d[tup[0]] = tup[1]
+        a.append(d)
+        
+    resultproxy.close()  
+    
+    db_conn.close()  
+    
+    return a
+    
+def getCustomer(username, password):
+    # conexion a la base de datos
+    db_conn = db_engine.connect()
+
+    query="select * from customers where username='" + username + "' and password='" + password + "'"
+    res=db_conn.execute(query).first()
+    
+    db_conn.close()  
+
+    if res is None:
+        return None
+    else:
+        return {'firstname': res['firstname'], 'lastname': res['lastname']}
+    
+def delCustomer(customerid, bFallo, bSQL, duerme, bCommit):
+    
+    # Array de trazas a mostrar en la página
+    dbr=[]
+
+    # TODO: Ejecutar consultas de borrado
+    # - ordenar consultas según se desee provocar un error (bFallo True) o no
+    # - ejecutar commit intermedio si bCommit es True
+    # - usar sentencias SQL ('BEGIN', 'COMMIT', ...) si bSQL es True
+    # - suspender la ejecución 'duerme' segundos en el punto adecuado para forzar deadlock
+    # - ir guardando trazas mediante dbr.append()
+    
+    try:
+        # TODO: ejecutar consultas
+
+    except Exception as e:
+        # TODO: deshacer en caso de error
+
+    else:
+        # TODO: confirmar cambios si todo va bien
+
+        
+    return dbr
+
